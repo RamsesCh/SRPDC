ab391c44ee9408bf252deac318410b66e004f75e
jchavarria <ramses.chav@gmail.com>
2021-12-08 23:41:10 -0600

    fix conexxion 2

--- Backend/conexion.php
+++ Backend/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli=new mysqli ("localhost", "root", "", "bd_srpdc");
+$mysqli=new mysqli ("localhost", "root", "password", "bd_srpdc");
 	if($mysqli->connect_errno){
 		echo "fallo al conectar: (".$mysqli->connect_errno.")".$mysqli
 		->connect_error;
